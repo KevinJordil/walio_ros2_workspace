721f49700184ab3f344fb86255f5bde0311c9323
KevinJordil <kevin.jordil@outlook.com>
2024-04-17 14:25:06 +0200

    basic_pg implement

--- src/motion_control/src/motion_control_node.cpp
+++ src/motion_control/src/motion_control_node.cpp
@@ -50,9 +50,11 @@ public:
                 RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Init service call was successful");
             } else {
                 RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Init service call was not successful");
+                return false;
             }
         } else {
             RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call init service");
+            return false;
         }
 
         // Wait 1 second to be sure that the device is ready
@@ -91,40 +93,40 @@ public:
             RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Reset driver service call was successful");
         } else {
             RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Reset driver service call error");
+            return false;
         }
 
         // Wait 2 seconds to be sure that the device is in a right state
         rclcpp::sleep_for(std::chrono::seconds(2));
 
-        // Create a request to set the state of driver in CSV mode
-        auto request = std::make_shared<opencn_communication_interfaces::srv::Pins::Request>();
+        // Create a request to set the parameters
+        auto first_request = std::make_shared<opencn_communication_interfaces::srv::Pins::Request>();
 
         // Array of Pin
-        std::vector<Pin> request_pins;
+        std::vector<Pin> first_request_pins;
 
-        // Set all pins to the desired state
-        Pin request_pin1;
-        request_pin1.pin_class = Pin::CMPINU32;
-        request_pin1.cmpinu32.value = 1;
-        request_pin1.name = "mux.target.selector";
-        request_pin1.transaction_type = Pin::SET;
-        request_pins.push_back(request_pin1);
+        Pin first_request_pin1;
+        first_request_pin1.pin_class = Pin::CMPINFLOAT;
+        first_request_pin1.cmpinfloat.value = 3000;
+        first_request_pin1.name = "basic_pg.best_pg.joint0.max_speed";
+        first_request_pin1.transaction_type = Pin::SET;
+        first_request_pins.push_back(first_request_pin1);
 
-        Pin request_pin2;
-        request_pin2.pin_class = Pin::CMPINBIT;
-        request_pin2.cmpinbit.value = 1;
-        request_pin2.name = "simple_pg.enable";
-        request_pin2.transaction_type = Pin::SET;
-        request_pins.push_back(request_pin2);
+        Pin first_request_pin2;
+        first_request_pin2.pin_class = Pin::CMPINFLOAT;
+        first_request_pin2.cmpinfloat.value = 100;
+        first_request_pin2.name = "basic_pg.best_pg.joint0.max_accel";
+        first_request_pin2.transaction_type = Pin::SET;
+        first_request_pins.push_back(first_request_pin2);
 
-        Pin request_pin3;
-        request_pin3.pin_class = Pin::CMPINBIT;
-        request_pin3.cmpinbit.value = 1;
-        request_pin3.name = "lcec.0.EPOS4.set-mode-csv";
-        request_pin3.transaction_type = Pin::SET;
-        request_pins.push_back(request_pin3);
+        Pin first_request_pin3;
+        first_request_pin3.pin_class = Pin::CMPINBIT;
+        first_request_pin3.cmpinbit.value = 1;
+        first_request_pin3.name = "basic_pg.best_pg.enable";
+        first_request_pin3.transaction_type = Pin::SET;
+        first_request_pins.push_back(first_request_pin3);
 
-        request->pins = request_pins;
+        first_request->pins = first_request_pins;
 
         // Wait for the service to be available
         while (!pin_client->wait_for_service(std::chrono::seconds(1))) {
@@ -136,15 +138,15 @@ public:
         }
 
         // Call the service
-        auto future_result = pin_client->async_send_request(request);
+        auto first_future_result = pin_client->async_send_request(first_request);
         
         // Wait for the result.
-        if(future_result.valid()){
-            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "CSV mode service call was successful");
+        if(first_future_result.valid()){
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Parameters service call was successful");
         } else {
-            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "CSV mode service call error");
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Parameters service call error");
+            return false;
         }
-
 
         return true;
     }
@@ -303,20 +305,23 @@ private:
     // Joy callback
     void joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg)
     {
-        if(old_button_status != msg->buttons[0]){
-            auto request = std::make_shared<opencn_communication_interfaces::srv::Pins::Request>();
+        //RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Axis %d: %f", 1, msg->axes[1]);   
+        float future_speed = msg->axes[1] * 1000;
+        if(current_speed != future_speed && ((current_speed + 10 < future_speed) || (current_speed - 10 > future_speed) || (future_speed < 10 && future_speed > -10))){
+            current_speed = future_speed;
+            auto first_request = std::make_shared<opencn_communication_interfaces::srv::Pins::Request>();
 
             // Array of Pin
-            std::vector<Pin> request_pins;
+            std::vector<Pin> first_request_pins;
 
-            Pin request_pin1;
-            request_pin1.pin_class = Pin::CMPINFLOAT;
-            request_pin1.cmpinfloat.value = msg->buttons[0] * 50;
-            request_pin1.name = "simple_pg.joint1.cmd-pos";
-            request_pin1.transaction_type = Pin::SET;
-            request_pins.push_back(request_pin1);
+            Pin first_request_pin1;
+            first_request_pin1.pin_class = Pin::CMPINFLOAT;
+            first_request_pin1.cmpinfloat.value = current_speed;
+            first_request_pin1.name = "basic_pg.best_pg.joint0.cmd_in";
+            first_request_pin1.transaction_type = Pin::SET;
+            first_request_pins.push_back(first_request_pin1);
 
-            request->pins = request_pins;
+            first_request->pins = first_request_pins;
 
             // Wait for the service to be available
             while (!pin_client->wait_for_service(std::chrono::seconds(1))) {
@@ -328,17 +333,16 @@ private:
             }
 
             // Call the service
-            auto future_result = pin_client->async_send_request(request);
+            auto first_future_result = pin_client->async_send_request(first_request);
             
             // Wait for the result.
-            if(future_result.valid()){
+            if(first_future_result.valid()){
                 RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Service call was successful");
             } else {
                 RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Service call error");
             }
 
-
-            old_button_status = msg->buttons[0];
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "speed set: %f", current_speed);   
 
 
         }
@@ -353,7 +357,7 @@ private:
     }
     rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_sub;
 
-    int old_button_status = 0;
+    float current_speed = 0;
 
     // Init service
     rclcpp::Client<opencn_communication_interfaces::srv::Init>::SharedPtr init_client;
